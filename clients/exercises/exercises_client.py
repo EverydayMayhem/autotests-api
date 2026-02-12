216bd737147a4dd1b6bb1fa6f7b649337c08178d
EverydayMayhem <lt26iw@gmail.com>
2026-02-12 11:35:05 +0300

    exercises api client

---
+++ clients/exercises/exercises_client.py
@@ -0,0 +1,89 @@
+from clients.api_client import APIClient
+from httpx import Response
+from typing import TypedDict
+
+
+class GetExerciseQueryDict(TypedDict):
+    """
+    Структура query-параметров для получения всех занятий по курсу
+    """
+    courseId: str
+
+
+class CreateExerciseRequestDict(TypedDict):
+    """
+    Структура тела запроса для создания занятий
+    """
+    title: str
+    courseId: str
+    maxScore: int
+    minScore: int
+    orderIndex: int
+    description: str
+    estimatedTime: str
+
+
+class UpdateExerciseRequestDict(TypedDict):
+    """
+    Структура тела запроса для обновления занятия
+    """
+    title: str | None
+    maxScore: int | None
+    minScore: int | None
+    orderIndex: int | None
+    description: str | None
+    estimatedTime: str | None
+
+
+class ExercisesClient(APIClient):
+    """
+    Класс для работы с /exercise
+    """
+
+    def get_exercises_api(self, query: GetExerciseQueryDict) -> Response:
+        """
+        Метод для получения заданий по айди курса
+
+        :param query: параметры запроса, courseId - айди курса по которому получаем список занятий
+        :return: возвращает объект типа httpx.Response
+        """
+        return self.get('/api/v1/exercises', params=query)
+
+    def create_exercise_api(self, request: CreateExerciseRequestDict) -> Response:
+        """
+        Метод для создания заданий
+
+        :param request: тело запроса с обязательными полями title, courseId,
+        maxScore, minScore, orderIndex, description, estimatedTime
+        :return: возвращает объект типа httpx.Response
+        """
+        return self.post('/api/v1/exercises', json=request)
+
+    def update_exercise_api(self, exercise_id: str, request: UpdateExerciseRequestDict) -> Response:
+        """
+        Метод для частичного обновления задания
+
+        :param exercise_id: идентификатор занятия
+        :param request: тело запроса с обязательными полями title,
+        maxScore, minScore, orderIndex, description, estimatedTime
+        :return: возвращает объект типа httpx.Response
+        """
+        return self.patch(f'/api/v1/exercises/{exercise_id}', json=request)
+
+    def get_exercise_api(self, exercise_id: str) -> Response:
+        """
+        Метод получения задания по id
+
+        :param exercise_id: идентификатор занятия
+        :return: возвращает объект типа httpx.Response
+        """
+        return self.get(f'/api/v1/exercises/{exercise_id}')
+
+    def delete_exercise_api(self, exercise_id: str) -> Response:
+        """
+        Метод удаления задания по id
+
+        :param exercise_id: идентификатор занятия
+        :return: возвращает объект типа httpx.Response
+        """
+        return self.delete(f'/api/v1/exercises/{exercise_id}')
---
+++ httpx_create_file.py
@@ -0,0 +1,38 @@
+import httpx
+from tools.fakers import get_random_email
+
+base_url = 'http://localhost:8000/api/v1'
+payload_create = {
+  "email": get_random_email(),
+  "password": "string",
+  "lastName": "string",
+  "firstName": "string",
+  "middleName": "string"
+}
+
+response_create = httpx.post(f'{base_url}/users', json=payload_create)
+response_create_body = response_create.json()
+print(response_create_body)
+print(response_create.status_code)
+
+payload_login = {
+  "email": payload_create['email'],
+  "password": payload_create['password']
+}
+
+response_login = httpx.post(f'{base_url}/authentication/login', json=payload_login)
+response_login_body = response_login.json()
+print(f'Login data: {response_login_body}')
+print(f'Статус код: {response_login.status_code}')
+
+create_file_headers = {
+    "Authorization": f"Bearer {response_login_body['token']['accessToken']}"
+}
+create_file_response = httpx.post(
+    f'{base_url}/files',
+    data={"filename": "cat.jpg", "directory": "images"},
+    files={"upload_file": open('./testdata/files/cat.jpg', 'rb')},
+    headers=create_file_headers
+                                  )
+create_file_response_data = create_file_response.json()
+print('Create file data: ', create_file_response_data)
