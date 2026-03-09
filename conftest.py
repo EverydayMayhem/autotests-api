d577467087bb0c1e5fc9d4b85efdb08f228f1b8d
EverydayMayhem <EverydayMayhem@users.noreply.github.com>
2026-03-09 09:44:02 +0000

    deploy: cca464243ed115055668baaff41415f02033d875

--- conftest.py
+++
@@ -1,8 +0,0 @@
-pytest_plugins = [
-    'fixtures.users',
-    'fixtures.authenticate',
-    'fixtures.files',
-    'fixtures.courses',
-    'fixtures.exercises',
-    'fixtures.allure'
-]
--- fixtures/authenticate.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from clients.auth_client.auth_client import get_auth_client, AuthClient
-
-
-@pytest.fixture
-def public_auth_client() -> AuthClient:
-    return get_auth_client()
