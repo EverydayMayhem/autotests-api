833ee947c142854b8fcbefb1fa1bbc3f2f4278d1
EverydayMayhem <lt26iw@gmail.com>
2026-03-14 19:23:29 +0300

    preps for Jenkins deployment

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
         extra='allow',
         env_file=".env",
         env_file_encoding="utf-8",
-        env_nested_delimiter=".",
+        env_nested_delimiter="__",
     )
     test_data: TestDataConfig
     http_client: HTTPClientConfig
